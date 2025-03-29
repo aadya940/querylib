fd26e15bf9967d153bf129baa1c0c256d632a252
Aadya Chinubhai <aadyachinubhai@gmail.com>
2025-03-30 00:46:30 +0530

    second commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from querylib import DocumentationRAG
 import pprint
 
-r = DocumentationRAG("sklearn.json", embedding_file="sklearn_embeddings.npy", similarity_top_k=8)
+r = DocumentationRAG("sklearn.json", similarity_top_k=8)
 res = r.ask_query("How to perform Simple Linear Regression using sklearn?")
 for i in res["results"]:
     pprint.pprint(i)
